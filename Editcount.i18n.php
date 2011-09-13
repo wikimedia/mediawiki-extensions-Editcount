33c94cfe680db71fe213f553342c95de94e166fa
Raimond Spekking <raymond@users.mediawiki.org>
2011-09-13 19:32:43 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- Editcount.i18n.php
+++ Editcount.i18n.php
@@ -179,7 +179,7 @@ $messages['bn'] = array(
 	'editcount' => 'সম্পাদনা সংখ্যা',
 	'editcount-desc' => 'ব্যবহারকারীর [[Special:Editcount|সম্পাদনা সংখ্যা]] দেখাও',
 	'editcount_username' => 'ব্যবহারকারী:',
-	'editcount_submit' => 'পেশ করো',
+	'editcount_submit' => 'জমা দাও',
 	'editcount_total' => 'সর্বমোট',
 );
 
